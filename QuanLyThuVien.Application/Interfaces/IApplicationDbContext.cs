448cc6aa222aa9c95a03188c70d2eac1daed2121
trungnguyenvan <trungnguyenvan90@gmail.com>
2026-08-10 12:53:58 +0700

    update tiếp

--- QuanLyThuVien.Application/Interfaces/IApplicationDbContext.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace QuanLyThuVien.Application.Interfaces
-{
-    public interface IApplicationDbContext
-    {
-        // Khai báo hàm lưu thay đổi bất đồng bộ
-        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);  // Lưu thay đổi vào cơ sở dữ liệu
-    }
-}
